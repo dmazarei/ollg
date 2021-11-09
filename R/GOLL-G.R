df7fcf608e752a0767713dc98e3a49a7ca62cde4
dmazarei <danial.mazarei@gmail.com>
2021-11-09 11:38:36 +0330

    change document

--- R/GOLL-G.R
+++ R/GOLL-G.R
@@ -1,15 +1,15 @@
 #' Generalized Odd log-logistic family of distributions (GOLL-G)
 #'
-#'  Distribution function, density, quantile function, hazard
-#'  function and random generation for Generalized Odd log-logistic family
-#'  of distributions (GOLL-G) with baseline cdf G.
+#' Computes the pdf, cdf, hdf, quantile and random numbers of the beta extended distribution due to Cordeiro et al. (2017) specified by the pdf
+#' \deqn{f(x)=\frac{\alpha\beta\,g(x)\,G(x)^{\alpha\beta-1}\left[1-G(x)^\alpha\right]^{\beta-1}}{\left[G(x)^{\alpha\beta}+\left[1-G(x)^\alpha\right]^\beta\right]^2}}
+#' for \eqn{G} any valid cdf, \eqn{g} the corresponding pdf,  \eqn{\alpha > 0}, the first shape parameter, and \eqn{\beta > 0}, the second shape parameter.
 #'
 #' @name GOLLG
-#' @param x,q A numeric/quantiles	vector.
-#' @param n number of observations. If \code{length(n) > 1},
-#'  the length is taken to be the number required.
-#' @param alpha non-negative parameter.
-#' @param beta non-negative parameter.
+#' @param x scaler or vector of values at which the pdf or cdf needs to be computed.
+#' @param q scaler or vector of probabilities at which the quantile needs to be computed, must be between 0 and 1 - exp(-alpha).
+#' @param n number of random numbers to be generated.
+#' @param alpha the value of the first shape parameter, must be positive, the default is 1.
+#' @param beta the value of the second shape parameter, must be positive, the default is 1.
 #' @param G A baseline continuous cdf.
 #' @param ... The baseline cdf parameters.
 #' @return  \code{pgollg} gives the distribution function,
